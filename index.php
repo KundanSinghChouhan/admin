500b6b77813f2136441de51ebf3410d18d10727e
KundanSinghChouhan <133588986+KundanSinghChouhan@users.noreply.github.com>
2023-06-20 11:42:09 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,42 @@
+<p>
+    <title>Will VPN Installation</title>
+</p>
+<style>
+    body {
+        text-align: center;
+        padding: 150px;
+    }
+
+    h1 {
+        font-size: 50px;
+    }
+
+    body {
+        font: 20px Helvetica, sans-serif;
+        color: #333;
+    }
+
+    article {
+        display: block;
+        text-align: left;
+        width: 650px;
+        margin: 0 auto;
+    }
+
+    a {
+        color: #dc8100;
+        text-decoration: none;
+    }
+
+    a:hover {
+        color: #333;
+        text-decoration: none;
+    }
+</style>
+<article>
+    <h2>Dear Client For installing the Will_Dev Script | Just Click on <a href="/install/"><strong><u>Installation Click</u></strong></a></h2>
+    <div>
+        <p>Before Click on Installation, Make Sure you have Read the Documentation, Inside Documentation All Steps is already mentoined, So, kindly Follow that, Or If you are getting any issue in Setup, Please Create Support Ticket, So Our Team will Help You. <a href="https://help.willdev.in/" rel="noopener noreferrer" target="_blank">https://help.willdev.in/</a></p>&mdash; The Team <a href="https://codecanyon.net/user/will_dev" rel="noopener noreferrer" target="_blank">Will_Dev</a>
+        <p><br></p>
+    </div>
+</article>
---
+++ pros_willdev_servers.php
@@ -0,0 +1,42 @@
+<p>
+    <title>Plugin Installation</title>
+</p>
+<style>
+    body {
+        text-align: center;
+        padding: 150px;
+    }
+
+    h1 {
+        font-size: 50px;
+    }
+
+    body {
+        font: 20px Helvetica, sans-serif;
+        color: #333;
+    }
+
+    article {
+        display: block;
+        text-align: left;
+        width: 650px;
+        margin: 0 auto;
+    }
+
+    a {
+        color: #dc8100;
+        text-decoration: none;
+    }
+
+    a:hover {
+        color: #333;
+        text-decoration: none;
+    }
+</style>
+<article>
+    <h2>Dear Client, For Active Open Server Plugin, You need to contact us to Get Plugin in Free, Or you can Create Support Ticket.</h2>
+    <div>
+        <p><strong>Note :-</strong> If you Only Want to Use <a href="https://developer.oneconnect.top/packages/">OneConnect.top</a> Server Only, Then Not Required to use this Plugin, For More Information Please create a Support Ticket, So Our Team will Help You. <a href="https://help.willdev.in/" rel="noopener noreferrer" target="_blank">https://help.willdev.in/</a></p>&mdash; The Team <a href="https://codecanyon.net/user/will_dev" rel="noopener noreferrer" target="_blank">Will_Dev</a>
+        <p>&mdash; Back to Panel <a href="../home.php" rel="noopener noreferrer" target="_blank">Back to Panel</a></p>
+    </div>
+</article>
